dba7241b1e3af82e4a25409243178e1ed721582a
Peter Siroki <310489+psiroki@users.noreply.github.com>
2021-12-30 16:05:58 +0100

    Moved the project to Dart 2 NNBD

--- lib/src/calculate_core/program.dart
+++ lib/src/calculate_core/program.dart
@@ -10,17 +10,37 @@ enum _Kind {
   closeBracket,
 }
 
+class CalculationException implements Exception {
+  CalculationException(this.message);
+
+  @override
+  String toString() => message;
+
+  final String message;
+}
+
+class CalculationParseException extends CalculationException {
+  CalculationParseException(String message, [this.buffer, this.offset])
+      : super(message);
+
+  final String? buffer;
+  final int? offset;
+}
+
 abstract class CalculationContext {
-  factory CalculationContext() => new _DefaultCalculationContext();
+  factory CalculationContext() => _DefaultCalculationContext();
   _StackElement pop();
   void push(_StackElement newElement);
 
-  num operator [](String registerName);
+  num? operator [](String registerName);
   void operator []=(String registerName, num value);
 }
 
 abstract class _StackCalculationContext implements CalculationContext {
+  @override
   _StackElement pop() => _stack.removeLast();
+
+  @override
   void push(_StackElement newElement) {
     _stack.add(newElement.bind(this));
   }
@@ -29,7 +49,10 @@ abstract class _StackCalculationContext implements CalculationContext {
 }
 
 class _DefaultCalculationContext extends _StackCalculationContext {
-  num operator [](String registerName) => _registers[registerName];
+  @override
+  num? operator [](String registerName) => _registers[registerName];
+
+  @override
   void operator []=(String registerName, num value) {
     _registers[registerName] = value;
   }
@@ -38,31 +61,36 @@ class _DefaultCalculationContext extends _StackCalculationContext {
 }
 
 class _OptimizationContext extends _StackCalculationContext {
+  @override
   num operator [](String registerName) {
     _registerAccess = true;
     return 1;
   }
 
+  @override
   void operator []=(String registerName, num value) {
     _registerAccess = true;
   }
 
+  @override
   _StackElement pop() {
-    _StackElement result = super.pop();
+    final _StackElement result = super.pop();
     _popped.add(result);
     return result;
   }
 
+  @override
   void push(_StackElement newElement) {
+    _StackElement elementToPush = newElement;
     if (_registerAccess) {
-      newElement = new _VariableReference("**unknown**", this);
+      elementToPush = _VariableReference("**unknown**", this);
       _registerAccess = false;
     }
     _popped.clear();
-    super.push(newElement);
+    super.push(elementToPush);
   }
 
-  num get stackDepth => _stack.length;
+  int get stackDepth => _stack.length;
   bool get stackIsNotEmpty => _stack.isNotEmpty;
   bool get topIsLiteral => _stack.last is _Literal;
   num get topValue => (_stack.last as _Literal).value;
@@ -70,31 +98,32 @@ class _OptimizationContext extends _StackCalculationContext {
   Iterable<_StackElement> stackSince(int start) => _stack.skip(start);
 
   bool _registerAccess = false;
-  List<_StackElement> _popped = [];
+  final List<_StackElement> _popped = [];
 }
 
 abstract class _StackElement {
   num get value;
-  void set value(num val);
+  set value(num val);
   _Opcode toOpcode();
 
   _StackElement bind(CalculationContext context);
 }
 
 class _VariableReference extends _StackElement {
-  _VariableReference(this.name, [this.context]);
+  _VariableReference(this.name, this.context);
 
+  @override
   num get value => context[name] ?? 0;
-  void set value(num val) {
-    context[name] = val;
-  }
 
-  _Opcode toOpcode() => new _Opcode()
-    ..kind = _Kind.identifier
-    ..value = name;
+  @override
+  set value(num val) => context[name] = val;
 
+  @override
+  _Opcode toOpcode() => _Opcode(_Kind.identifier)..value = name;
+
+  @override
   _VariableReference bind(CalculationContext context) =>
-      new _VariableReference(name, context);
+      _VariableReference(name, context);
 
   final String name;
   final CalculationContext context;
@@ -103,16 +132,17 @@ class _VariableReference extends _StackElement {
 class _Literal extends _StackElement {
   _Literal(this._value);
 
+  @override
   num get value => _value;
 
-  void set value(num val) {
-    throw "You can't set a value to a literal";
-  }
+  @override
+  set value(num val) =>
+      throw CalculationException("You can't set a value to a literal");
 
-  _Opcode toOpcode() => new _Opcode()
-    ..kind = _Kind.number
-    ..value = _value;
+  @override
+  _Opcode toOpcode() => _Opcode(_Kind.number)..value = _value;
 
+  @override
   _Literal bind(CalculationContext context) => this;
 
   final num _value;
@@ -122,67 +152,74 @@ typedef void _PerformOperation(_Opcode op, CalculationContext n);
 typedef _Opcode _ToOpcode(_Token token);
 
 class _KindDefinition {
-  _KindDefinition(this.kind, this.singleCharacter,
-      [this.perform, _ToOpcode toOpcode])
-      : this._toOpcodeOverride = toOpcode;
+  _KindDefinition(
+    this.kind,
+    this.singleCharacter, [
+    this.perform,
+    _ToOpcode? toOpcode,
+  ]) : _toOpcodeOverride = toOpcode;
 
   _Opcode toOpcode(_Token token) {
-    if (_toOpcodeOverride != null) return _toOpcodeOverride(token);
-    return new _Opcode()
-      ..kind = token.kind
-      ..value = token.value;
+    if (_toOpcodeOverride != null) return _toOpcodeOverride!(token);
+    return _Opcode(token.kind)..value = token.value;
   }
 
   final _Kind kind;
-  final String singleCharacter;
-  final _PerformOperation perform;
-  final _ToOpcode _toOpcodeOverride;
+  final String? singleCharacter;
+  final _PerformOperation? perform;
+  final _ToOpcode? _toOpcodeOverride;
 }
 
-final List<_KindDefinition> _tokenDefinitions = new List.unmodifiable([
-  new _KindDefinition(_Kind.add, "+", (_Opcode op, CalculationContext n) {
-    n.push(new _Literal(n.pop().value + n.pop().value));
+final List<_KindDefinition> _tokenDefinitions = List.unmodifiable([
+  _KindDefinition(_Kind.add, "+", (_Opcode op, CalculationContext n) {
+    n.push(_Literal(n.pop().value + n.pop().value));
   }),
-  new _KindDefinition(_Kind.subtract, "-", (_Opcode op, CalculationContext n) {
-    n.push(new _Literal(-n.pop().value + n.pop().value));
+  _KindDefinition(_Kind.subtract, "-", (_Opcode op, CalculationContext n) {
+    n.push(_Literal(-n.pop().value + n.pop().value));
   }),
-  new _KindDefinition(_Kind.multiply, "*", (_Opcode op, CalculationContext n) {
-    n.push(new _Literal(n.pop().value * n.pop().value));
+  _KindDefinition(_Kind.multiply, "*", (_Opcode op, CalculationContext n) {
+    n.push(_Literal(n.pop().value * n.pop().value));
   }),
-  new _KindDefinition(_Kind.divide, "/", (_Opcode op, CalculationContext n) {
-    num a = n.pop().value, b = n.pop().value;
-    n.push(new _Literal(b / a));
+  _KindDefinition(_Kind.divide, "/", (_Opcode op, CalculationContext n) {
+    final num a = n.pop().value;
+    final num b = n.pop().value;
+    n.push(_Literal(b / a));
   }),
-  new _KindDefinition(_Kind.number, null, (_Opcode op, CalculationContext n) {
-    n.push(new _Literal(op.value as num));
-  },
-      (_Token token) => new _Opcode()
-        ..kind = token.kind
-        ..value = num.parse(token.value)),
-  new _KindDefinition(_Kind.identifier, null,
-      (_Opcode op, CalculationContext n) {
-    n.push(new _VariableReference(op.value));
+  _KindDefinition(
+    _Kind.number,
+    null,
+    (_Opcode op, CalculationContext n) {
+      n.push(_Literal(op.value! as num));
+    },
+    (_Token token) => _Opcode(token.kind)..value = num.parse(token.value!),
+  ),
+  _KindDefinition(_Kind.identifier, null, (_Opcode op, CalculationContext n) {
+    n.push(_VariableReference(op.value! as String, n));
   }),
-  new _KindDefinition(_Kind.assign, "=", (_Opcode op, CalculationContext n) {
-    num value = n.pop().value;
-    _VariableReference target = n.pop() as _VariableReference;
+  _KindDefinition(_Kind.assign, "=", (_Opcode op, CalculationContext n) {
+    final num value = n.pop().value;
+    final _VariableReference target = n.pop() as _VariableReference;
     target.value = value;
-    n.push(new _Literal(value));
+    n.push(_Literal(value));
   }),
-  new _KindDefinition(_Kind.openBracket, "("),
-  new _KindDefinition(_Kind.closeBracket, ")"),
+  _KindDefinition(_Kind.openBracket, "("),
+  _KindDefinition(_Kind.closeBracket, ")"),
 ]);
-final Map<_Kind, _KindDefinition> _kindDefs = new Map.unmodifiable(
-    new Map.fromIterable(_tokenDefinitions, key: (t) => t.kind));
+final Map<_Kind, _KindDefinition> _kindDefs = Map.unmodifiable(
+  Map.fromEntries(_tokenDefinitions.map((def) => MapEntry(def.kind, def))),
+);
 
-final Map<String, _Kind> _singleCharacterTokens = new Map.unmodifiable(
-    new Map.fromIterable(
-        _kindDefs.keys.where((k) => _kindDefs[k].singleCharacter != null),
-        key: (k) => _kindDefs[k].singleCharacter));
+final Map<String, _Kind> _singleCharacterTokens = Map.unmodifiable(
+  Map.fromEntries(
+    _kindDefs.entries.where((entry) => entry.value.singleCharacter != null).map(
+          (entry) => MapEntry(entry.value.singleCharacter, entry.value.kind),
+        ),
+  ),
+);
 
 String kindToString(_Kind kind) {
-  String kindString = kind.toString();
-  int dot = kindString.indexOf('.');
+  final String kindString = kind.toString();
+  final int dot = kindString.indexOf('.');
   if (dot < 0) return kindString;
   return kindString.substring(dot + 1);
 }
@@ -192,78 +229,100 @@ abstract class _Kindful {
 }
 
 class _Token implements _Kindful {
-  String toString() => "${kindToString(kind)}(${value??''})";
-  _Opcode toOpcode() => _kindDefs[kind].toOpcode(this);
+  _Token(this.kind);
+
+  @override
+  String toString() => "${kindToString(kind)}(${value ?? ''})";
+  _Opcode toOpcode() => _kindDefs[kind]!.toOpcode(this);
+
+  @override
   _Kind kind;
-  String value;
+
+  String? value;
 }
 
 class _Opcode implements _Kindful {
-  String toString() => "${kindToString(kind)}${value!=null?' ':''}${value??''}";
+  _Opcode(this.kind);
+
+  @override
+  String toString() =>
+      "${kindToString(kind)}${value != null ? ' ' : ''}${value ?? ''}";
   void perform(CalculationContext context) =>
-      _kindDefs[kind].perform(this, context);
+      _kindDefs[kind]!.perform!(this, context);
+
+  @override
   _Kind kind;
-  Object value;
+
+  Object? value;
 }
 
-final Map<_Kind, RegExp> _patternTokens = new Map.unmodifiable({
-  _Kind.number: new RegExp(r"(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?"),
-  _Kind.identifier: new RegExp(r"[_a-zA-Z][_a-zA-Z0-9]*"),
+final Map<_Kind, RegExp> _patternTokens = Map.unmodifiable({
+  _Kind.number: RegExp(r"(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?"),
+  _Kind.identifier: RegExp(r"[_a-zA-Z][_a-zA-Z0-9]*"),
 });
 
 Iterable<_Token> _tokenize(String buffer) sync* {
   final int l = buffer.length;
   for (int i = 0; i < l; ++i) {
     if (buffer[i] == " ") continue;
-    _Kind single = _singleCharacterTokens[buffer[i]];
+    final _Kind? single = _singleCharacterTokens[buffer[i]];
     if (single == null) {
       bool found = false;
-      for (_Kind k in _patternTokens.keys) {
-        RegExp re = _patternTokens[k];
+      for (final _Kind k in _patternTokens.keys) {
+        final RegExp re = _patternTokens[k]!;
         if (buffer.startsWith(re, i)) {
           found = true;
-          String match = re.stringMatch(buffer.substring(i));
-          yield new _Token()
-            ..kind = k
-            ..value = match;
+          final String match = re.stringMatch(buffer.substring(i))!;
+          yield _Token(k)..value = match;
           i += match.length - 1;
           break;
         }
       }
-      if (found == null) throw "Found a strange character in the source";
+      if (!found) {
+        throw CalculationParseException(
+          "Found a strange character in the source at $i: ${buffer[i]}",
+          buffer,
+          i,
+        );
+      }
     } else {
-      yield new _Token()..kind = single;
+      yield _Token(single)..kind;
     }
   }
 }
 
 int _valueExpression(List<_Opcode> ops, List<_Token> tokens, int cursor) {
-  _Token current = tokens[cursor];
+  int currentCursor = cursor;
+  final _Token current = tokens[currentCursor];
   if (current.kind == _Kind.openBracket) {
-    cursor = _expression.expression(ops, tokens, cursor + 1);
-    if (tokens[cursor].kind != _Kind.closeBracket)
-      throw "Ouch, no closing bracket";
-    return cursor + 1;
+    currentCursor = _expression.expression(ops, tokens, currentCursor + 1);
+    if (tokens[currentCursor].kind != _Kind.closeBracket) {
+      throw CalculationParseException("Ouch, no closing bracket");
+    }
+    return currentCursor + 1;
   }
   if (current.kind == _Kind.identifier) {
-    ops.add(tokens[cursor].toOpcode());
+    ops.add(tokens[currentCursor].toOpcode());
   } else {
-    if (current.kind == _Kind.subtract) ++cursor;
-    assert(tokens[cursor].kind == _Kind.number);
-    ops.add((tokens[cursor]
-      ..value = "${current.kind == _Kind.subtract ? '-' : ''}"
-          "${tokens[cursor].value}").toOpcode());
+    if (current.kind == _Kind.subtract) ++currentCursor;
+    assert(tokens[currentCursor].kind == _Kind.number);
+    ops.add(
+      (tokens[currentCursor]
+            ..value = "${current.kind == _Kind.subtract ? '-' : ''}"
+                "${tokens[currentCursor].value}")
+          .toOpcode(),
+    );
   }
-  return cursor + 1;
+  return currentCursor + 1;
 }
 
 class _PrecedenceGroup {
   _PrecedenceGroup(Iterable<_Kind> kinds, [this.parent])
-      : this.kinds = kinds.toSet();
+      : kinds = kinds.toSet();
 
   int halfExpression(List<_Opcode> ops, List<_Token> tokens, int cursor) {
-    _Token opToken = null;
-    if (this.kinds.contains(tokens[cursor].kind)) {
+    _Token? opToken;
+    if (kinds.contains(tokens[cursor].kind)) {
       // E => E + T
       opToken = tokens[cursor];
     } else {
@@ -274,27 +333,30 @@ class _PrecedenceGroup {
     return expression(ops, tokens, cursor + 1, opToken);
   }
 
-  int expression(List<_Opcode> ops, List<_Token> tokens, int cursor,
-      [_Token lastTokenOp]) {
-    int afterTerm = parent == null
+  int expression(
+    List<_Opcode> ops,
+    List<_Token> tokens,
+    int cursor, [
+    _Token? lastTokenOp,
+  ]) {
+    final int afterTerm = parent == null
         ? _valueExpression(ops, tokens, cursor)
-        : parent.expression(ops, tokens, cursor);
+        : parent!.expression(ops, tokens, cursor);
     if (lastTokenOp != null) ops.add(lastTokenOp.toOpcode());
-    if (afterTerm < tokens.length)
+    if (afterTerm < tokens.length) {
       return halfExpression(ops, tokens, afterTerm);
+    }
     return afterTerm;
   }
 
-  _PrecedenceGroup over(Iterable<_Kind> kinds) =>
-      new _PrecedenceGroup(kinds, this);
+  _PrecedenceGroup over(Iterable<_Kind> kinds) => _PrecedenceGroup(kinds, this);
 
-  final _PrecedenceGroup parent;
+  final _PrecedenceGroup? parent;
   final Set<_Kind> kinds;
 }
 
-_PrecedenceGroup _expression =
-    new _PrecedenceGroup([_Kind.multiply, _Kind.divide])
-        .over([_Kind.add, _Kind.subtract]).over([_Kind.assign]);
+_PrecedenceGroup _expression = _PrecedenceGroup([_Kind.multiply, _Kind.divide])
+    .over([_Kind.add, _Kind.subtract]).over([_Kind.assign]);
 
 int _parseTokens(List<_Opcode> ops, List<_Token> tokens, int cursor) {
   return _expression.expression(ops, tokens, cursor);
@@ -306,12 +368,12 @@ class Program {
     _parseTokens(_ops, _tokenize(source).toList(growable: false), 0);
   }
 
-  Program._fromOps(List<_Opcode> ops) : _ops = new List.unmodifiable(ops);
+  Program._fromOps(List<_Opcode> ops) : _ops = List.unmodifiable(ops);
 
   /// Executes the program in the given calculaton context.
-  num execute([CalculationContext context]) {
-    if (context == null) context = new CalculationContext();
-    for (_Opcode op in _ops) {
+  num execute([CalculationContext? context]) {
+    context ??= CalculationContext();
+    for (final _Opcode op in _ops) {
       op.perform(context);
     }
     return context.pop().value;
@@ -320,9 +382,9 @@ class Program {
   /// Returns an optimized version of the program if possible.
   /// Tries to calculate as much as it can in advance.
   Program optimize() {
-    _OptimizationContext context = new _OptimizationContext();
+    final _OptimizationContext context = _OptimizationContext();
     // new, optimized opcodes
-    List<_Opcode> newOps = [];
+    final List<_Opcode> newOps = [];
     // the bound to which we have already added the opcodes
     // we don't automatically add opcodes for contextfree calculations,
     // only when needed, but we need to save where we have to
@@ -331,12 +393,13 @@ class Program {
     // we save the stack from the last written opcode before the operation,
     // so if the operation taints the stack, we emit this, and emit the
     // opcode that tainted the stack
-    List<_StackElement> stackSave = null;
-    for (_Opcode op in _ops) {
-      if (context.stackIsNotEmpty && context.topIsLiteral)
+    List<_StackElement>? stackSave;
+    for (final _Opcode op in _ops) {
+      if (context.stackIsNotEmpty && context.topIsLiteral) {
         stackSave = context.stackSince(stackBound).toList(growable: false);
-      else
+      } else {
         stackSave = null;
+      }
       op.perform(context);
       if (!context.topIsLiteral) {
         // the expression has referred to a variable
@@ -351,13 +414,11 @@ class Program {
     }
     if (context.topIsLiteral) {
       // the expression refers to no variable
-      return new Program._fromOps([
-        new _Opcode()
-          ..kind = _Kind.number
-          ..value = context.topValue
-      ]);
+      return Program._fromOps(
+        [_Opcode(_Kind.number)..value = context.topValue],
+      );
     }
-    if (newOps.length < _ops.length) return new Program._fromOps(newOps);
+    if (newOps.length < _ops.length) return Program._fromOps(newOps);
     // could not optimize
     return this;
   }
@@ -366,6 +427,7 @@ class Program {
 
   /// Provides a stirng representation of the program by listing
   /// the opcodes seperated by newlines.
+  @override
   String toString() => _ops.join("\n");
 
   List<_Opcode> _ops = [];
--- test/calculate_test.dart
+++ test/calculate_test.dart
@@ -8,79 +8,99 @@ class Pair<A, B> {
   final B second;
 }
 
-void main(List<String> args) {
+void main() {
   test("Very simple calculation", () {
-    Program simple = new Program("2+2");
+    final Program simple = Program("2+2");
     expect(simple.execute(), equals(4));
   });
 
   test("Simple assignment", () {
-    Program simple = new Program("pi=3.141592654");
-    CalculationContext context = new CalculationContext();
+    final Program simple = Program("pi=3.141592654");
+    final CalculationContext context = CalculationContext();
     expect(simple.execute(context), equals(3.141592654));
     expect(context["pi"], equals(3.141592654));
   });
 
   test("Multiple assignments and simple calculations", () {
-    CalculationContext context = new CalculationContext();
-    List<Pair<Program, num>> pairs = [
-      new Pair(new Program("a=3"), 3),
-      new Pair(new Program("b=5"), 5),
-      new Pair(new Program("a*b"), 15),
+    final CalculationContext context = CalculationContext();
+    final List<Pair<Program, num>> pairs = [
+      Pair(Program("a=3"), 3),
+      Pair(Program("b=5"), 5),
+      Pair(Program("a*b"), 15),
     ];
-    for (Pair<Program, num> pair in pairs)
+    for (final Pair<Program, num> pair in pairs) {
       expect(pair.first.execute(context), equals(pair.second));
+    }
     expect(context["a"], equals(3));
     expect(context["b"], equals(5));
   });
 
   test("Operator precedence", () {
-    Program prec = new Program("-2+-3*7/(2+2*0.5-0.5)-5");
+    final Program prec = Program("-2+-3*7/(2+2*0.5-0.5)-5");
     expect(prec.execute(), equals(-15.4));
   });
 
   test("Simple optimization: no variables", () {
-    Program prec = new Program("-2+-3*7/(2+2*0.5-0.5)-5");
-    Program opt = prec.optimize();
+    final Program prec = Program("-2+-3*7/(2+2*0.5-0.5)-5");
+    final Program opt = prec.optimize();
     expect(opt.execute(), equals(-15.4), reason: "The result is correct");
-    expect(prec.numOpcodes, greaterThan(1),
-        reason:
-            "The test is incorrect: originally there was more than one opcode");
-    expect(opt.numOpcodes, equals(1),
-        reason: "The final number of opcodes is not exactly one");
+    expect(
+      prec.numOpcodes,
+      greaterThan(1),
+      reason:
+          "The test is incorrect: originally there was more than one opcode",
+    );
+    expect(
+      opt.numOpcodes,
+      equals(1),
+      reason: "The final number of opcodes is not exactly one",
+    );
   });
 
   test("Simple optimization with tailing variables", () {
-    Program prec = new Program("2+2+a+b");
-    Program opt = prec.optimize();
-    CalculationContext context = new CalculationContext();
+    final Program prec = Program("2+2+a+b");
+    final Program opt = prec.optimize();
+    final CalculationContext context = CalculationContext();
     context["a"] = 5;
     context["b"] = 3;
     expect(opt.execute(context), equals(12), reason: "The result is incorrect");
-    expect(opt.numOpcodes, lessThan(prec.numOpcodes),
-        reason: "The number of opcodes has not decreased");
+    expect(
+      opt.numOpcodes,
+      lessThan(prec.numOpcodes),
+      reason: "The number of opcodes has not decreased",
+    );
   });
 
   test("Tail optimization", () {
-    Program prec = new Program("a+b+(2+2)");
-    Program opt = prec.optimize();
-    CalculationContext context = new CalculationContext();
+    final Program prec = Program("a+b+(2+2)");
+    final Program opt = prec.optimize();
+    final CalculationContext context = CalculationContext();
     context["a"] = 5;
     context["b"] = 3;
     expect(opt.execute(context), equals(12), reason: "The result is incorrect");
-    expect(opt.numOpcodes, lessThan(prec.numOpcodes),
-        reason: "The number of opcodes has not decreased");
+    expect(
+      opt.numOpcodes,
+      lessThan(prec.numOpcodes),
+      reason: "The number of opcodes has not decreased",
+    );
   });
 
   test("Complex optimization", () {
-    String source = "7*2.5+a*b+(3+2+2*a-7*8)";
-    Program prec = new Program(source);
-    Program opt = prec.optimize();
-    CalculationContext context = new CalculationContext();
+    const String source = "7*2.5+a*b+(3+2+2*a-7*8)";
+    final Program prec = Program(source);
+    final Program opt = prec.optimize();
+    final CalculationContext context = CalculationContext();
     context["a"] = 5;
     context["b"] = 3;
-    expect(opt.execute(context), equals(-8.5), reason: "The result is incorrect");
-    expect(opt.numOpcodes, lessThan(prec.numOpcodes),
-        reason: "The number of opcodes has not decreased");
+    expect(
+      opt.execute(context),
+      equals(-8.5),
+      reason: "The result is incorrect",
+    );
+    expect(
+      opt.numOpcodes,
+      lessThan(prec.numOpcodes),
+      reason: "The number of opcodes has not decreased",
+    );
   });
 }
